94b73db5a4c39f9a8481844b6548a5f311bb4fa0
Rishabh-1999 <rishabhanand33@gmail.com>
2019-05-04 22:03:10 +0530

    New Page

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(()=>{
+    var typed=new Typed('.typed',{
+        strings : ['Student.','Web Developer.','Android Deveoper','Coder.','Indian'],
+        typeSpeed : 70,
+        loop : true,
+        startDelay : 1000,
+        showCursor : false
+    });
+
+    
+});

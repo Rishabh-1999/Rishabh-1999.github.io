77617bc9da87bb85a7c05d4680f6a89b4c783076
Rishabh-1999 <rishabhanand33@gmail.com>
2019-05-20 12:13:32 +0530

    Addition of active Nav

--- script.js
+++ script.js
@@ -20,3 +20,41 @@ $(document).keydown(function(e){
        return false;
     }
 });
+$(document).mousemove(function(e){
+    var section1=document.getElementById("section1");
+    var section2=document.getElementById("section2");
+    var section3=document.getElementById("section3");
+    var section4=document.getElementById("section4");
+    section1.onmouseover = function(e) {
+        document.getElementById("homenav").style.fontWeight = "700";
+        document.getElementById("homenav").style.textDecoration =  "underline";
+    };
+    section1.onmouseout = function(e) {
+       document.getElementById("homenav").style.fontWeight = "400";
+        document.getElementById("homenav").style.textDecoration =  "none";
+    };
+    section2.onmouseover = function(e) {
+        document.getElementById("aboutmenav").style.fontWeight = "700";
+        document.getElementById("aboutmenav").style.textDecoration =  "underline";
+    };
+    section2.onmouseout = function(e) {
+        document.getElementById("aboutmenav").style.fontWeight = "400";
+        document.getElementById("aboutmenav").style.textDecoration =  "none";
+    };
+    section3.onmouseover = function(e) {
+        document.getElementById("techiusenav").style.fontWeight = "700";
+        document.getElementById("techiusenav").style.textDecoration =  "underline";
+    };
+    section3.onmouseout = function(e) {
+        document.getElementById("techiusenav").style.fontWeight = "400";
+        document.getElementById("techiusenav").style.textDecoration =  "none";
+    };
+    section4.onmouseover = function(e) {
+        document.getElementById("impprojectnav").style.fontWeight = "700";
+        document.getElementById("impprojectnav").style.textDecoration =  "underline";
+    };
+    section4.onmouseout = function(e) {
+        document.getElementById("impprojectnav").style.fontWeight = "400";
+        document.getElementById("impprojectnav").style.textDecoration =  "none";
+    };
+});

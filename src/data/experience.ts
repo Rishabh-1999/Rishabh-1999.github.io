9c3a2f3bc4ddd442e804063a410ed292689c1768
Rishabh Anand <38128234+Rishabh-1999@users.noreply.github.com>
2023-01-23 22:31:53 +0530

    Job Change Updation

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,78 +1,100 @@
 import PreScienceLogo from "assets/images/prescience.png";
+import AutodeskLogo from "assets/images/autodesk.png";
 
 export interface WorkExperiencesDataType {
-    position: string;
-    companyName: string;
-    companyLink: string;
-    logo: any;
-    from: string;
-    to?: string;
-    descriptions?: (string | string[])[];
-    skills?: string[];
+  position: string;
+  companyName: string;
+  companyLink: string;
+  logo: any;
+  from: string;
+  to?: string;
+  descriptions?: (string | string[])[];
+  skills?: string[];
 }
 
 const ExperienceData: WorkExperiencesDataType[] = [
-    {
-        position: "Full Stack Developer",
-        companyName: "PreScience Decision Solutions",
-        companyLink: "https://www.prescienceds.com",
-        logo: PreScienceLogo,
-        from: "2021-08-01",
-        descriptions: [
-            [
-                "• Build various custom Dashboard/Report in Web, PDF, and Excel for various Clients like Banks, and B2B.",
-                "•	Have developed various REST APIs using Node JS, Spring Boot, and Flask for multiple interfaces under various projects.",
-                "•	Have led a team for multiple projects, which has increased the growth of the Company to a new extent.",
-                "• Created standardized code structures for writing code, testing, and deployment for React JS, Node JS projects",
-                "• Developed various chatbots using ReactJS, Node JS, Socket.io, Google TTS and integrated with Rasa / Dialog flow for various clients",
-                "• Responsible for managing VM(s) and deploying various applications using containers",
-                "• Developed custom-made WordPress Plugins using ReactJS for various Clients",
-                "• Interacted with various clients for various projects as a project lead/manager",
-                "• Built innovative React library page builder which creates a page visually by dragging only",
-            ],
-            "Apart from my responsibilities, I am responsible for guiding other juniors in the design and development of web applications projects.",
-        ],
-        skills: [
-            "React",
-            "Node JS",
-            "JavaScript",
-            "TypeScript",
-            "Docker",
-            "Flask",
-            "Python",
-            "Spring",
-            "Java",
-            "MySQL",
-            "Postgres",
-            "Figma",
-            "Git",
-        ],
-    },
-    {
-        position: "Full Stack Intern",
-        companyName: "PreScience Decision Solutions",
-        companyLink: "https://www.prescienceds.com/",
-        logo: PreScienceLogo,
-        from: "2020-06-08",
-        to: "2021-07-30",
-        descriptions: [
-            [
-                "• Responsible for design and development of web applications using modern UI design systems with expandable, scalable, and secured backend",
-                "• Developed chatbots using ReactJS, Socket.io, and integrated with Rasa",
-                "• Responsible for managing VM(s) and deploying various applications using containers",
-                "• Modified existing applications to detect and correct errors, improve performance, and upgrade interfaces.",
-            ],
-        ],
-        skills: [
-            "React",
-            "Node JS",
-            "JavaScript",
-            "Docker",
-            "MySQL",
-            "Figma",
-            "Git",
-        ],
-    },
+  {
+    position: "Software Enginner - II",
+    companyName: "Autodesk",
+    companyLink: "https://www.autodesk.com/",
+    logo: AutodeskLogo,
+    from: "2022-12-12",
+    descriptions: [""],
+    skills: [
+      "React",
+      "Node JS",
+      "GraphQL",
+      "JavaScript",
+      "TypeScript",
+      "Docker",
+      "Postgres",
+      "Figma",
+      "Jira",
+      "Git",
+    ],
+  },
+  {
+    position: "Full Stack Developer",
+    companyName: "PreScience Decision Solutions",
+    companyLink: "https://www.prescienceds.com",
+    logo: PreScienceLogo,
+    from: "2021-08-01",
+    to: "2022-12-08",
+    descriptions: [
+      [
+        "• Build various custom Dashboard/Report in Web, PDF, and Excel for various Clients like Banks, and B2B.",
+        "•	Have developed various REST APIs using Node JS, Spring Boot, and Flask for multiple interfaces under various projects.",
+        "•	Have led a team for multiple projects, which has increased the growth of the Company to a new extent.",
+        "• Created standardized code structures for writing code, testing, and deployment for React JS, Node JS projects",
+        "• Developed various chatbots using ReactJS, Node JS, Socket.io, Google TTS and integrated with Rasa / Dialog flow for various clients",
+        "• Responsible for managing VM(s) and deploying various applications using containers",
+        "• Developed custom-made WordPress Plugins using ReactJS for various Clients",
+        "• Interacted with various clients for various projects as a project lead/manager",
+        "• Built innovative React library page builder which creates a page visually by dragging only",
+      ],
+      "Apart from my responsibilities, I am responsible for guiding other juniors in the design and development of web applications projects.",
+    ],
+    skills: [
+      "React",
+      "Node JS",
+      "JavaScript",
+      "TypeScript",
+      "Docker",
+      "Flask",
+      "Python",
+      "Spring",
+      "Java",
+      "MySQL",
+      "Postgres",
+      "Figma",
+      "Git",
+    ],
+  },
+  {
+    position: "Full Stack Intern",
+    companyName: "PreScience Decision Solutions",
+    companyLink: "https://www.prescienceds.com/",
+    logo: PreScienceLogo,
+    from: "2020-06-08",
+    to: "2021-07-30",
+    descriptions: [
+      [
+        "• Responsible for design and development of web applications using modern UI design systems with expandable, scalable, and secured backend",
+        "• Developed chatbots using ReactJS, Socket.io, and integrated with Rasa",
+        "• Responsible for managing VM(s) and deploying various applications using containers",
+        "• Modified existing applications to detect and correct errors, improve performance, and upgrade interfaces.",
+      ],
+    ],
+    skills: [
+      "React",
+      "Node JS",
+      "JavaScript",
+      "Docker",
+      "MySQL",
+      "Figma",
+      "Git",
+    ],
+  },
 ];
 
 export default ExperienceData;
--- src/sections/LandingPage/index.tsx
+++ src/sections/LandingPage/index.tsx
@@ -14,107 +14,102 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MyPhoto from "assets/images/me/MyPhoto.webp";
 
 const typewriterSteps = [
-    ["Full Stack Developer  ⚛️", 2500],
-    ["Software Engineer🧑🏻‍💻", 2500],
-    ["Defense Expert 🪖⚔️", 2500],
-    ["Anime Lover", 1000],
-    ["Indian 🕉️ 🇮🇳", 2500],
+  ["Full Stack Developer  ⚛️", 2500],
+  ["Software Engineer🧑🏻‍💻", 2500],
+  ["Defense Expert 🪖⚔️", 2500],
+  ["Anime Lover", 1000],
+  ["Indian 🕉️ 🇮🇳", 2500],
 ];
 
 function LandingPage() {
-    return (
-        <section id="landing_page" className="snap-start">
-            <NavBar />
+  return (
+    <section id="landing_page" className="snap-start">
+      <NavBar />
 
-            <div className="max-w-6xl mx-auto xl:px-16 md:px-8 px-2 md:pt-20 pt-12">
-                <div className="flex flex-wrap items-start md:justify-start justify-center">
-                    <motion.div
-                        initial={{
-                            x: -100,
-                            opacity: 0.5,
-                        }}
-                        animate={{ x: 0, opacity: 1, scale: 1 }}
-                        transition={{ duration: 0.25 }}
-                        className="left md:pt-10"
-                    >
-                        <h1 className="font-extrabold text-5xl leading-10">
-                            Rishabh
-                            <br />
-                            Anand
-                            <span className="text-highlightColor">.</span>
-                        </h1>
-                        <div className="h-[3px] w-[110px] mt-6 mb-4 bg-highlightColor"></div>
+      <div className="max-w-6xl mx-auto xl:px-16 md:px-8 px-2 md:pt-20 pt-12">
+        <div className="flex flex-wrap items-start md:justify-start justify-center">
+          <motion.div
+            initial={{
+              x: -100,
+              opacity: 0.5,
+            }}
+            animate={{ x: 0, opacity: 1, scale: 1 }}
+            transition={{ duration: 0.25 }}
+            className="left md:pt-10"
+          >
+            <h1 className="font-extrabold text-5xl leading-10">
+              Rishabh
+              <br />
+              Anand
+              <span className="text-highlightColor">.</span>
+            </h1>
+            <div className="h-[3px] w-[110px] mt-6 mb-4 bg-highlightColor"></div>
 
-                        <Typical
-                            steps={typewriterSteps.flat()}
-                            loop={Infinity}
-                        />
-                    </motion.div>
+            <Typical steps={typewriterSteps.flat()} loop={Infinity} />
+          </motion.div>
 
-                    <motion.div
-                        initial={{
-                            y: -100,
-                            opacity: 0,
-                        }}
-                        animate={{ y: 0, opacity: 1, scale: 1 }}
-                        transition={{ duration: 0.5 }}
-                        className="flex flex-grow justify-center items-end"
-                    >
-                        <img
-                            className="max-w-full h-[500px] object-cover"
-                            src={MyPhoto}
-                            alt="Rishabh Anand"
-                            loading="eager"
-                        />
-                    </motion.div>
+          <motion.div
+            initial={{
+              y: -100,
+              opacity: 0,
+            }}
+            animate={{ y: 0, opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            className="flex flex-grow justify-center items-end"
+          >
+            <img
+              className="max-w-full h-[500px] object-cover"
+              src={MyPhoto}
+              alt="Rishabh Anand"
+              loading="eager"
+            />
+          </motion.div>
 
-                    <motion.div
-                        initial={{
-                            x: +100,
-                            opacity: 0.5,
-                        }}
-                        animate={{ x: 0, opacity: 1, scale: 1 }}
-                        transition={{ duration: 0.25 }}
-                        className="right md:pt-10 md:block hidden"
-                    >
-                        <h3 className="font-bold text-[1.75rem] text-left text-highlightColor tracking-wide">
-                            INTRODUCTION
-                        </h3>
-                        <h3 className="font-bold text-[1.1rem]">
-                            Full Stack Developer&nbsp;
-                            <br />
-                            <a
-                                href="https://www.prescienceds.com/"
-                                target="_blank"
-                                rel="noreferrer"
-                                className="tracking-wide"
-                            >
-                                @&nbsp;PreScience DS
-                            </a>
-                        </h3>
-                        <p className=" break-normal">
-                            React JS | Node JS | Flask | Docker
-                        </p>
+          <motion.div
+            initial={{
+              x: +100,
+              opacity: 0.5,
+            }}
+            animate={{ x: 0, opacity: 1, scale: 1 }}
+            transition={{ duration: 0.25 }}
+            className="right md:pt-10 md:block hidden"
+          >
+            <h3 className="font-bold text-[1.75rem] text-left text-highlightColor tracking-wide">
+              INTRODUCTION
+            </h3>
+            <h3 className="font-bold text-[1.1rem]">
+              Software Enginner&nbsp;
+              <br />
+              <a
+                href="https://www.prescienceds.com/"
+                target="_blank"
+                rel="noreferrer"
+                className="tracking-wide"
+              >
+                @&nbsp;Autodesk
+              </a>
+            </h3>
+            <p className=" break-normal">React JS | Node JS | Flask | Docker</p>
 
-                        <a
-                            href="#about-me"
-                            className="text-highlightColor tracking-wide flex items-end text-[1.1rem] font-bold"
-                        >
-                            Learn more
-                            <ExpandMoreIcon />
-                        </a>
-                    </motion.div>
-                </div>
+            <a
+              href="#about-me"
+              className="text-highlightColor tracking-wide flex items-end text-[1.1rem] font-bold"
+            >
+              Learn more
+              <ExpandMoreIcon />
+            </a>
+          </motion.div>
+        </div>
 
-                {/* <ReactAudioPlayer
+        {/* <ReactAudioPlayer
                     src={background__music}
                     autoPlay
                     volume={0.3}
                     muted
                 /> */}
-            </div>
-        </section>
-    );
+      </div>
+    </section>
+  );
 }
 
 export default LandingPage;
